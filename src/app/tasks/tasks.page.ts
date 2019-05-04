2f31b92d4cc82d1db83073bb67502d530663aae6
Thierry Bitz <thierry.bitz@students.fhnw.ch>
2019-05-04 20:23:00 +0200

    changes in home and tasks

--- src/app/tasks/tasks.page.ts
+++ src/app/tasks/tasks.page.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TasksPage implements OnInit {
 
-  constructor() { }
+  searchQuery: string = '';
+  items: string[];
 
-  ngOnInit() {
+  constructor() {
+    this.initializeItems();
   }
 
+  ngOnInit() {
+
+  }
+
+  initializeItems() {
+    this.items = [
+      'Amsterdam',
+      'Bogota',
+      'Berlin',
+      'Medellin'
+    ];
+  }
+
+  getItems(ev: any) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the searchbar
+    const val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() !== '') {
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
 }
